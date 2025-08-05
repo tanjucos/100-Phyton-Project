c04dbebe964ae5681ccc0a5b5b0421a535043b0a
tanjuco <tanju.coskun@rwth-aachen.de>
2025-08-05 21:15:44 +0200

    001_Band Name Generator

---
+++ Band Name Generator.py
@@ -0,0 +1,5 @@
+greeting = "Hello Mate! Its time to create a Band Name"
+print(greeting)
+question_one = input("Whats the name of your city?")
+question_two = input("If you had a pet, what was it name?")
+print("Your band name could be..." + question_one + " " +  question_two)
